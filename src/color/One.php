80f526e3e7165f58b3196955a3ee1931526b3f44
Warner <934797303@qq.com>
2019-08-26 11:11:59 +0800

    init && dev package

---
+++ src/color/One.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace color;
+
+class One
+{
+    public function index()
+    {
+        echo __METHOD__ . PHP_EOL;
+    }
+}
---
+++ src/color/Two.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace color;
+
+class Two
+{
+    public function index()
+    {
+        echo __METHOD__ . PHP_EOL;
+    }
+}
